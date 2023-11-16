9d6ac56a7199ea12e198eff149b2789bf38912b9
cshimvin <>
2023-11-16 21:27:14 +0000

    Add main function

--- run.py
+++ run.py
@@ -57,6 +57,12 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated!\n")
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+
+main()
